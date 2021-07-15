d89ea096b41ae6c961b2c1add862224fdfdd4dd1
RyoMikami <mikamiryomyself@gmail.com>
2021-07-16 03:59:06 +0900

    first commit

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 # written by Ryo Mikami
 
 # import 
-import os
 import streamlit as st
+import os
 import numpy as np
 import pandas as pd
-from PIL  import Image
+from PIL  import Image 
 #pip freeze| grep numpy
 
 #生データの読み込み
